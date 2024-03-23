1206bb681edf325161ee421df6977313ad23fac8
Diana W <windemut@uw.edu>
2024-03-23 11:34:07 -0700

    change default to daily

--- src/weather.py
+++ src/weather.py
@@ -9,18 +9,22 @@ WEATHER_ATTRIBUTES = [
 ]
 NWS_URL = "https://api.weather.gov/points"
 
-def call_nws(lat_lon: Tuple[float, float], is_hourly: bool = True) -> dict:
+
+def call_nws(lat_lon: Tuple[float, float], is_hourly: bool = False) -> dict:
     url = f"{NWS_URL}/{lat_lon[0]},{lat_lon[1]}"
     response = requests.get(url)
     if is_hourly:
         granularity = "forecastHourly"
-        n_units = 24 * 7
+        n_units = None
     else:
         granularity = "forecast"
         n_units = 2 * 7
     sub_url = response.json()["properties"][granularity]
     sub_response = requests.get(sub_url)
+    n_units = n_units or len(sub_response.json()["properties"]["periods"])
     forecast = [{k: sub_response.json()["properties"]["periods"][unit_from_now][k] for k in WEATHER_ATTRIBUTES} for unit_from_now in range(n_units)]
     return {lat_lon: forecast}
+
+print(call_nws((47.4867, -121.7535)))
 
 
